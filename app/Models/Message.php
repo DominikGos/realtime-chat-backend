bc60e810b83aeb28c4d35ca1ba69ac2ad2bed6a8
Dominik <dominikgos3@gmail.com>
2023-10-14 21:28:34 +0200

    added relations and set fillable property in models

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Message extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'text'
+    ];
+
+    public function files(): HasMany
+    {
+        return $this->hasMany(MessageFile::class);
+    }
+
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
